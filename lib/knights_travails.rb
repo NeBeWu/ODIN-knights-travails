5eb74882783139283e21bd13499459f2499e4bf7
Pedro <nebewu@gmail.com>
2022-04-06 19:36:33 -0300

    Implement the knight_moves method
    Add a moves method which outputs all possible moves the knight can
    do from a given position. Add a search_path method which searches
    recursively posssible paths between two positions. Finally,
    implement the knight_moves method using the search_path method.

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -21,12 +21,39 @@ class Knight
   def initialize(board, position = [0, 0])
     @board = board
     @position = position
+    @move_directions = [[2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]]
   end
 
-  def possible_move?(position)
-    return false if @board.[](*position).nil?
+  def possible_move?(move, position = @position)
+    return false if @board.[](*move).nil?
 
-    (position.first - @position.first).abs == 1 && (position.last - @position.last).abs == 2 ||
-      (position.first - @position.first).abs == 2 && (position.last - @position.last).abs == 1
+    (move.first - position.first).abs == 1 && (move.last - position.last).abs == 2 ||
+      (move.first - position.first).abs == 2 && (move.last - position.last).abs == 1
+  end
+
+  def moves(position = @position)
+    @move_directions.map do |direction|
+      [position.first + direction.first, position.last + direction.last]
+    end.select do |move|
+      possible_move?(move, position)
+    end
+  end
+
+  # This method uses recursion to implement a breadth first search on
+  # the 'fictional' graph of paths to find a path between the origin
+  # (represented in the path variable first method call) and the
+  # destiny.
+  def search_path(destiny, path = [@position], queue = [])
+    return path if path.last == destiny
+
+    possible_paths = moves(path.last).map { |move| path + [move] }
+
+    queue.push(*possible_paths)
+
+    search_path(destiny, queue.shift, queue)
+  end
+
+  def knight_moves(origin, destiny)
+    search_path(destiny, [origin])
   end
 end
